0ce7f97ed5350fc8543836d0b62bfbeb93e8bc39
John Ordonez <72434368+jordo2k15@users.noreply.github.com>
2021-03-01 13:47:10 -0500

    Create App.js

---
+++ PokeApp/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+import SideBar from './SideBar';
+import PokemonList from './PokemonList';
+import logo from './logo.gif';
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+      <SideBar />
+      </div>
+
+)
+}
+}
+
+export default App;
